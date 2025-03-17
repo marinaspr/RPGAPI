18798b3f4d44a5975f5caef91f31edbf731f15bc
marinaspr <hyejuaya@gmail.com>
2025-03-17 09:01:03 -0300

    PAM mudanças

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 //conectar com o banco usando o que ta la no appsetings
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee"));
 });
 
 // Add services to the container.
